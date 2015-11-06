a2c4f30c5cbc1b6e0c943aa0fe00167f765213cb
lennart <lennart@neofonie.de>
2015-11-06 17:50:53 +0100

    fix for npm3 / missing dll for win build

--- grunt/nwjs.js
+++ grunt/nwjs.js
@@ -3,7 +3,9 @@ module.exports = function (grunt, options) {
     return {
         options: {
             platforms: ['win'],
-            buildDir: 'target/'
+            buildDir: 'target/',
+            // Issue with missing ffmpegsumo.dll -> https://github.com/nwjs/nw-builder/issues/252
+            version: 'v0.12.0'
         },
         buildApp: {
             src: ['target/preparedBuild/**']
