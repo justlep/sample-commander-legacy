77a41e41628560b4cefbdf61c26412f17e1c6689
Lennart Pegel <lennart@neofonie.de>
2016-08-31 10:45:11 +0200

    changed nwjs version

--- grunt/nwjs.js
+++ grunt/nwjs.js
@@ -5,7 +5,7 @@ module.exports = function (grunt, options) {
             platforms: ['win'],
             buildDir: 'target/',
             // Issue with missing ffmpegsumo.dll -> https://github.com/nwjs/nw-builder/issues/252
-            version: 'v0.12.0'
+            version: '0.12.3'
         },
         buildApp: {
             src: ['target/preparedBuild/**']
