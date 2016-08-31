916d6f229cd22244f02b8ebb7a778ad48b46a5e2
Lennart Pegel <lennart@neofonie.de>
2016-08-31 12:56:52 +0200

    fixed build version

--- grunt-tasks/copyPackageJson.js
+++ grunt-tasks/copyPackageJson.js
@@ -1,15 +1,19 @@
 module.exports = function (grunt) {
     'use strict';
 
-    var PACKAGE_JSON_SOURCE_PATH = 'src/package.json',
+    var ROOT_PACKAGE_JSON_PATH = 'package.json',
+        PACKAGE_JSON_SOURCE_PATH = 'src/package.json',
         PACKAGE_JSON_TARGET_PATH = 'build/preparedBuild/package.json';
 
     grunt.registerTask('copyPackageJson', 'Copies an altered package.json to the preparedBuild path.',
         function () {
-            var packageJson = grunt.file.readJSON(PACKAGE_JSON_SOURCE_PATH);
+            var rootPackageJson = grunt.file.readJSON(ROOT_PACKAGE_JSON_PATH),
+                packageJson = grunt.file.readJSON(PACKAGE_JSON_SOURCE_PATH),
+                version = rootPackageJson.version;
 
             packageJson.window.toolbar = false;
-            packageJson.window.title += ' v' + packageJson.version;
+            packageJson.version = version;
+            packageJson.window.title += ' v' + version;
 
             grunt.file.write(PACKAGE_JSON_TARGET_PATH, JSON.stringify(packageJson, null, 2));
 
