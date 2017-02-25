b157683782d0c0bd18996a7538cdf2a1654195c2
Lennart Pegel <github@justlep.net>
2017-02-25 19:27:09 +0100

    shell:runDebug task now system-independent

--- grunt/shell.js
+++ grunt/shell.js
@@ -2,11 +2,11 @@ module.exports = function (grunt /* ,options*/) {
     'use strict';
 
     var path = require('path'),
-        nwExecutablePath = path.join(__dirname, '../node_modules/nodewebkit/nodewebkit/nw.exe');
+        nwExecutablePath = path.join(__dirname, '../node_modules/nodewebkit/bin/nodewebkit');
 
     return {
         runDebug: {
-            command: nwExecutablePath + ' .',
+            command: 'node ' + nwExecutablePath + ' .',
             options: {
                 callback: function(err, stdout, stderr, cb) {
                     grunt.log.ok(stdout);
