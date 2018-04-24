adb0189161bb620fac21e7fbabeaa58b58ea8532
Lennart Pegel <github@justlep.net>
2018-04-25 00:48:22 +0200

    added tests for ffmpeg spectrograms

---
+++ utils/test-ffmpeg-spectrograms/fire.js
@@ -0,0 +1,38 @@
+
+const audioFilenames = ['sweep.wav', 'bing.wav'];
+const ffmpegExe = 'ffmpeg.exe';
+const WINDOW_FUNCS = [
+	 'rect'
+	,'bartlett'
+	,'hann'
+	,'hanning'
+	,'hamming'
+	,'blackman'
+	,'welch'
+	,'flattop'
+	,'bharris'
+	,'bnuttall'
+	,'bhann'
+	,'sine'
+	,'nuttall'
+	,'lanczos'
+	,'gauss'
+	,'tukey'
+	,'dolph'
+	,'cauchy'
+	,'parzen'
+	,'poisson'];
+
+// 
+let windowFuncs = ['hann','lanczos'];
+
+
+let nodeExec = require('child_process').execSync;
+
+audioFilenames.forEach(audioFilename => {
+	windowFuncs.forEach(winFunc => {
+		let spectroImgFilename = audioFilename + '-' + winFunc + '.png';
+		let cmd = ffmpegExe + ` -y -i "${audioFilename}" -lavfi showspectrumpic=s=800x400:color=fire:legend=0:win_func=${winFunc},format=yuv444p "${spectroImgFilename}"`;
+		nodeExec(cmd);
+	});
+});
