d45756b6e82eea60405a57aa0d8e031ad99edf51
Lennart Pegel <github@justlep.net>
2018-04-28 23:41:12 +0200

    dummy code

--- utils/test-ffmpeg-spectrograms/fire.js
+++ utils/test-ffmpeg-spectrograms/fire.js
@@ -1,5 +1,5 @@
 
-const audioFilenames = ['sweep.wav', 'bing.wav', 'sweep-linear.wav'];
+const audioFilenames = ['siren.wav']; // ['sweep.wav', 'bing.wav', 'sweep-linear.wav'];
 const ffmpegExe = 'ffmpeg.exe';
 const WINDOW_FUNCS = [
 	 'rect'
@@ -24,15 +24,17 @@ const WINDOW_FUNCS = [
 	,'poisson'];
 
 // 
-let windowFuncs = ['hann','lanczos'];
-
+let windowFuncs = ['hann'];
+let ampScales = ['lin','sqrt','cbrt','log','4thrt','5thrt'];
 
 let nodeExec = require('child_process').execSync;
 
 audioFilenames.forEach(audioFilename => {
-	windowFuncs.forEach(winFunc => {
-		let spectroImgFilename = audioFilename + '-' + winFunc + '.png';
-		let cmd = ffmpegExe + ` -y -i "${audioFilename}" -lavfi showspectrumpic=s=800x400:color=fire:legend=0:win_func=${winFunc},format=yuv444p "${spectroImgFilename}"`;
-		nodeExec(cmd);
-	});
+    ampScales.forEach(ampScale => {
+        windowFuncs.forEach(winFunc => {
+            let spectroImgFilename = audioFilename + '-' + winFunc + '-' + ampScale + '.png';
+            let cmd = ffmpegExe + ` -y -i "${audioFilename}" -lavfi showspectrumpic=s=800x800:color=fire:scale=${ampScale}:legend=0:win_func=${winFunc} "${spectroImgFilename}"`;
+            nodeExec(cmd);
+        });
+    });
 });
